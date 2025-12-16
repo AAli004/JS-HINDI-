d462c6a2d83c4a132935caef1281b7d4267bd83b
AAli004 <asrafasifali112005@gmail.com>
2025-12-16 11:57:23 +0530

    String Conversion

---
+++ 03.1_ConversionOperation.js
@@ -0,0 +1,59 @@
+// ******************* OPERATIONS ******************
+
+let value = 6
+let negValue = -value
+    // console.log(negValue);
+
+/* console.log(2 + 2);
+console.log(5 - 6);
+console.log(8 - 3);
+console.log(2 * 2);
+console.log(2 ** 9);
+console.log(10 / 2);
+console.log(2 / 3);
+console.log(2 % 3);
+console.log(10 % 4); */
+
+// *********** Addition Of String ***********
+
+let str1 = "Hello"
+let str2 = " Asif"
+    // console.log(str1 + str2);
+let str3 = str1 + str2
+console.log(str3);
+
+/* console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+
+console.log("1" + 2 + 2);
+console.log("1" + "2" + 2);
+console.log(2 + 2 + "1");
+console.log(2 + "2" + "1");
+
+console.log(3 + 4 * 5 % 3); // It is not good manner , use always brackets
+console.log((3 + 4) * 5 % 3); */
+
+/* console.log(true);
+console.log(+true); // = > 1
+// console.log(true-) = > error
+console.log(-true); // = > -1
+// console.log(true - );  = > error
+console.log(-false); // = > -0
+console.log(+false); // = > 0
+console.log(+""); // = > 0
+console.log(-""); // => -0 */
+
+console.log("O".repeat(50));
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter1 = 100
+    ++gameCounter1;
+console.log(gameCounter1);
