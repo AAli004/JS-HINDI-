31a9818f625076aaf93d3c5eeb9f916cbd9cf618
AAli004 <asrafasifali112005@gmail.com>
2025-12-18 08:52:19 +0530

    Comparision Of Data type

---
+++ 04_Comparision.js
@@ -0,0 +1,36 @@
+/* console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 2);
+console.log(2 != 1); */
+/*console.log("Asif" > "Ali");
+console.log("Ali" > "Asif");
+console.log("Asif" > "asif");
+console.log("A" > "a");
+console.log("02" > 1);
+console.log("2" > 1); */
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+console.log(null != 0);
+
+console.log("*".repeat(50));
+
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined != 0);
+
+console.log("^".repeat(60));
+
+
+// === , ==
+
+console.log("78" == 78);
+console.log("78" === 78);
