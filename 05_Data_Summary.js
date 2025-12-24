82ea99c2fa2a17ceca62a68e900877dceaf8fdd3
AAli004 <asrafasifali112005@gmail.com>
2025-12-24 17:27:34 +0530

    Data Type Summary 02 1

---
+++ 05_Data_Summary.js
@@ -0,0 +1,83 @@
+// #Primitive Data 
+// 7 Types : String , Number , Boolean , null , undefined , Symbol  , BigInt
+
+const score = 100 // Number
+const scoreValue = 100.3
+
+const isLoggedIn = false // Boolean 
+
+const outsideTemp = null // null 
+
+let user_Email; // value not assigned so, it is called undifined ( It is First Method )
+let userEmail = undefined // it is second method 
+
+const Id = Symbol('123') // Symbol
+const anotherId = Symbol('123')
+console.log(typeof Id);
+console.log(typeof anotherId);
+console.log(Id == anotherId);
+
+// const bigNumber = 45565569964664455558846556255587862654544 n // to use BigInt represent with 'n' at end of number it's first method
+// console.log(typeof bigNumber);
+const bigNumber1 = BigInt("446654645456546546454546545465123") // second method 
+console.log(typeof bigNumber1);
+
+const x = "456n" // String
+console.log(typeof x);
+console.log(typeof outsideTemp);
+// const bigNumber3 = BigInt("3456543576674356754")
+console.log(typeof bigNumber3);
+
+
+
+
+
+
+console.log("*".repeat(100));
+console.log("Reference Data ");
+
+
+
+
+
+// #Non-Primitive (Reference) Data Types
+// Array , objects , Function
+
+
+const broName = ["Ahsan", "Faisal", "Imaad", "Faizan", "Alam", "Areez"]; // Array
+
+
+const myObj = {
+    name: "Asif",
+    age: 22,
+}; // Object
+
+
+const myFunction = function() {
+    console.log("Hello World");
+
+
+}
+
+// console.log(myObj);
+// console.log(broName);
+// console.log(myFunction);
+// console.log(typeof myFunction);
+/*console.log(typeof broName); // result ------> object
+console.log(typeof myObj);   // result ------> object
+console.log(typeof myFunction); // result ------> Object function*/
+
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof [{}]);
+/* 
+if you want to check is your Array is Array or Not then use this method 
+*/
+console.log(Array.isArray(broName));
+console.log(typeof
+    function() {});
+console.log(typeof
+    function(myFunction) {});
+
+const bigNumber4 = BigInt("65645587955555145554")
+console.log(typeof bigNumber4);
