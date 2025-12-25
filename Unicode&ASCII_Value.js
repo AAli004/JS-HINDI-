e5f247d380a026bdcce42796f87fd7dc840e59c2
AAli004 <asrafasifali112005@gmail.com>
2025-12-25 09:15:00 +0530

    UNICODE/ASCII Value

---
+++ Unicode&ASCII_Value.js
@@ -0,0 +1,21 @@
+/* 
+(space)
+↓
+special characters
+↓
+numbers (0–9)
+↓
+capital letters (A–Z)
+↓
+small letters (a–z)
+↓
+other languages
+↓
+emoji 😄🔥🚀
+*/
+
+
+/*
+https://chatgpt.com/s/t_694cb00b95fc8191b9625818b2413875  // Chatgpt Link
+https://chatgpt.com/share/694cb26f-5f24-8005-a95a-d20a59b8a538  // ChatGPT Link
+*/
