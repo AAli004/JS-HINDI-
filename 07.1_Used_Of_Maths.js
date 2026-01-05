3fff4d131c754aef671d1619f2cf8a1d0aab8652
AAli004 <asrafasifali112005@gmail.com>
2026-01-05 21:32:50 +0530

    Most Important About Math

---
+++ 07.1_Used_Of_Maths.js
@@ -0,0 +1,16 @@
+const min_2 = 10
+const max_2 = 20
+console.log(Math.floor(Math.random() * 10 + 1) + min_2);
+
+
+
+
+/*
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+*/
+
+const min_1 = 9
+const max_1 = 99
+console.log(Math.floor(Math.random() * (max_1 - min_1 + 1) + min_1));
